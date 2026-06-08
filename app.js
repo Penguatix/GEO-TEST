f5360a4d9da4b597112cdf00af4dd086ab1c33c8
Supash <88264676+Penguatix@users.noreply.github.com>
2026-06-08 14:58:20 +0530

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- app.js
+++ app.js
@@ -37,13 +37,13 @@ let actualCoords = null;
 let resultsLayers = []; 
 let isHost = false; 
 
-// import { initializeAppCheck, ReCaptchaV3Provider } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app-check.js";
+import { initializeAppCheck, ReCaptchaV3Provider } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app-check.js";
 
 // NEW: ACTIVATE PUBLIC ATTIDUDINAL APP CHECK CHECKSUM GATE
-// const appCheck = initializeAppCheck(app, {
-//     provider: new ReCaptchaV3Provider('6LeQHRAtAAAAAJMGvjg5CxEiVJ_9MTspWKvkVCeu'),
-//     isTokenAutoRefreshEnabled: true // Automatically updates token keys invisibly mid-game session
-// });
+const appCheck = initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider('6LeQHRAtAAAAAJMGvjg5CxEiVJ_9MTspWKvkVCeu'),
+    isTokenAutoRefreshEnabled: true // Automatically updates token keys invisibly mid-game session
+});
 
 // Pool of global location metrics
 const gameLocations = [
